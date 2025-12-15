43310f6519f61ebd904ca33472b9ca06a7452851
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2025-12-15 09:12:17 -0500

    Import Sony Android 10 kernel changes
    Attempt, should be latest changes. Let me know if not.

--- drivers/bus/mhi/core/mhi_main.c
+++ drivers/bus/mhi/core/mhi_main.c
@@ -1,3 +1,8 @@
+/*
+ * NOTE: This file has been modified by Sony Mobile Communications Inc.
+ * Modifications are Copyright (c) 2020 Sony Mobile Communications Inc,
+ * and licensed under the license of the file.
+ */
 // SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2018-2020, The Linux Foundation. All rights reserved. */
 
@@ -1056,7 +1061,12 @@ static int parse_rsc_event(struct mhi_controller *mhi_cntrl,
 
 	result.transaction_status = (ev_code == MHI_EV_CC_OVERFLOW) ?
 		-EOVERFLOW : 0;
-	result.bytes_xferd = xfer_len;
+/* SONY_BEGIN (Changes related to CR#2677376) */
+	//result.bytes_xferd = xfer_len;
+
+	/* truncate to buf len if xfer_len is larger */
+	result.bytes_xferd = min_t(u16, xfer_len, buf_info->len);
+/* SONY_END (Changes related to CR#2677376) */
 	result.buf_addr = buf_info->cb_buf;
 	result.dir = mhi_chan->dir;
 
@@ -1304,10 +1314,15 @@ int mhi_process_data_event_ring(struct mhi_controller *mhi_cntrl,
 		mhi_event->last_dev_rp = (u64)dev_rp;
 
 		chan = MHI_TRE_GET_EV_CHID(local_rp);
+/* SONY_BEGIN (Changes related to CR#2617422) */
 		if (chan >= mhi_cntrl->max_chan) {
 			MHI_ERR("invalid channel id %u\n", chan);
-			continue;
-		}
+/* SONY_BEGIN (Changes related to CR#2673763) */
+			//continue;
+			goto next_er_element;
+/* SONY_END (Changes related to CR#2673763) */
+		 }
+/* SONY_END (Changes related to CR#2617422) */
 		mhi_chan = &mhi_cntrl->mhi_chan[chan];
 
 		if (likely(type == MHI_PKT_TYPE_TX_EVENT)) {
@@ -1317,7 +1332,9 @@ int mhi_process_data_event_ring(struct mhi_controller *mhi_cntrl,
 			parse_rsc_event(mhi_cntrl, local_rp, mhi_chan);
 			event_quota--;
 		}
-
+/* SONY_BEGIN (Changes related to CR#2673763) */
+next_er_element:
+/* SONY_END (Changes related to CR#2673763) */
 		mhi_recycle_ev_ring_element(mhi_cntrl, ev_ring);
 		local_rp = ev_ring->rp;
 		dev_rp = mhi_to_virtual(ev_ring, er_ctxt->rp);
