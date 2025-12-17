e1d7a550a416363bd75ab286e8f8ae3c29f612aa
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2025-12-17 08:03:35 -0500

    techpack: display: Import Sony changes
    Sony is based off of some commit before 10400, so I merged onto the previous commit, then fixed for 10400. Hope it works.

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -28,6 +28,11 @@
 
 #define DSI_MODE_MAX 32
 
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+#define SDE_PINCTRL_STATE_TOUCH_ACTIVE "somc_ts_int_active"
+#define SDE_PINCTRL_STATE_TOUCH_SUSPEND  "somc_ts_int_suspend"
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
+
 enum dsi_panel_rotation {
 	DSI_PANEL_ROTATE_NONE = 0,
 	DSI_PANEL_ROTATE_HV_FLIP,
@@ -89,6 +94,10 @@ struct dsi_pinctrl_info {
 	struct pinctrl *pinctrl;
 	struct pinctrl_state *active;
 	struct pinctrl_state *suspend;
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+	struct pinctrl_state *touch_state_active;
+	struct pinctrl_state *touch_state_suspend;
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
 };
 
 struct dsi_panel_phy_props {
@@ -208,6 +217,10 @@ struct dsi_panel {
 	int panel_test_gpio;
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
+
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+	struct panel_specific_pdata *spec_pdata;
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -277,6 +290,16 @@ int dsi_panel_set_lp1(struct dsi_panel *panel);
 int dsi_panel_set_lp2(struct dsi_panel *panel);
 
 int dsi_panel_set_nolp(struct dsi_panel *panel);
+
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+int dsi_panel_set_hbm_mode(struct dsi_panel *panel, int mode);
+
+int dsi_panel_set_flm2_mode(struct dsi_panel *panel, int mode);
+
+int dsi_panel_set_opec_mode(struct dsi_panel *panel, int mode);
+
+int dsi_panel_set_hmd_mode(struct dsi_panel *panel, int mode);
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
 
 int dsi_panel_prepare(struct dsi_panel *panel);
 
--- techpack/display/msm/msm_drv.c
+++ techpack/display/msm/msm_drv.c
@@ -48,6 +48,10 @@
 #include "sde_wb.h"
 #include "sde_dbg.h"
 
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+#include "dsi/dsi_panel_driver.h"
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
+
 /*
  * MSM driver version:
  * - 1.0.0 - initial interface
@@ -830,6 +834,10 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 	}
 
 	drm_kms_helper_poll_init(ddev);
+
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+	incell_driver_init(priv);
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
 
 	return 0;
 
