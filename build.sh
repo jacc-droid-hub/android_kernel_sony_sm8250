35bf3255886292c192ec911983a373959f602030
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2026-01-09 17:09:21 -0500

    Update build.sh for my setup

--- build.sh
+++ build.sh
@@ -2,15 +2,14 @@
 
 mkdir -p out
 
-GCC_PATH=/run/media/bbn/7b433792-2537-4661-97dd-14052ca9b4b9/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin
-CLANG_PATH=/run/media/bbn/7b433792-2537-4661-97dd-14052ca9b4b9/LineageOS/prebuilts/clang/host/linux-x86/clang-r353983c1/bin
+GCC_PATH=/home/fun-guy/android/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin
+CLANG_PATH=/home/fun-guy/android/lineage-17-stock/prebuilts/clang/host/linux-x86/clang-r353983c1/bin
 
 BUILD_CROSS_COMPILE=${GCC_PATH}/aarch64-linux-gnu-
 CLANG_TRIPLE=aarch64-linux-gnu-
 
-DEFCONFIG=kona-bbn_defconfig
+DEFCONFIG=pdx206_defconfig
 
-# export KBUILD_DIFFCONFIG=pdx203_j_diffconfig
 export PATH=${CLANG_PATH}:${PATH}
 
 if [[ -n "$1" ]]
@@ -26,5 +25,4 @@ else
     make -j16 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
         CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
         CROSS_COMPILE=$BUILD_CROSS_COMPILE 2>&1 | tee build.txt
-fi 
-# cp out/arch/arm64/boot/Image $(pwd)/boot.img-zImage
+fi
